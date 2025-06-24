c1e943c7c3f8ba1faae855c599da7db6c3829607
Arya Khairy <aryakhairy95@gmail.com>
2025-06-24 15:12:16 +0700

    Panduan papan tictactoe

--- src/TTTConsoleNonOO.java
+++ src/TTTConsoleNonOO.java
@@ -63,6 +63,16 @@ public class TTTConsoleNonOO {
         }
         currentPlayer = CROSS;   // cross plays first
         currentState  = PLAYING; // ready to play
+
+        System.out.println("Papan permainan dipetakan sebagai berikut:");
+        System.out.println(" 1 1 | 1 2 | 1 3 ");
+        System.out.println("-----+-----+-----");
+        System.out.println(" 2 1 | 2 2 | 2 3 ");
+        System.out.println("-----+-----+-----");
+        System.out.println(" 3 1 | 3 2 | 3 3 ");
+        System.out.println();
+
+
     }
 
     /** The currentPlayer makes one move (one step).
